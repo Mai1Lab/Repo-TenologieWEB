8c1a9349fffbce48ee550470b8cd6fba2483cc61
mai1lab <maione.enzo@gmail.com>
2025-04-10 08:22:33 +0200

    Aggiunta di file HTML e JavaScript per la gestione del DOM, inclusi alert e console log per il valore di un elemento.

---
+++ LEZIONI/46 - JAVASCRIPT ESEMPI/Dove Posizionare Javascript/script.js
@@ -0,0 +1,11 @@
+/*credo un alert che preleva il valore di un elemento
+ html con id= "mYp"  e lo mostra in un alert e in console
+
+ document.getElementById("myP"): cerca un elemento HTML con id="myP"
+ .innerHTML: prende il contenuto HTML interno di quell’elemento. ( compresi i taghtml)
+ alert(...): mostra una finestra di avviso (popup) con quel contenuto.
+*/
+
+
+alert(document.getElementById("myP").innerHTML)
+console.log("Text")
